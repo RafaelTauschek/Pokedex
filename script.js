370990783b71a42191f189dc537c194be5da4f1e
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-06-24 23:05:46 +0200

    rewrote a log

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let limit = 5;
+let limit = 20;
 let pokemonNames = [];
 let pokemonDetails = [];
 let pokemonSpecies = [];
@@ -6,250 +6,187 @@ let pokemonSpecies = [];
 
 async function init() {
     await loadAllPokemonData();
+    console.log(pokemonNames);
+    console.log(pokemonDetails);
+    console.log(pokemonSpecies);
     render();
 }
 
 
 async function loadAllPokemonData() {
-    await loadPokemon();
-    await loadSinglePokemon(pokemonNames);
+    await loadPokemonNames();
+    await loadPokemonDetails();
+    await loadPokemonSpecies();
 }
 
 
-async function loadPokemon() {
-    let cacheKey = `pokemon_name`;
-    if (localStorage.getItem(cacheKey)) {
-        console.log('Fetching from cache - Name');
-        pokemonNames = JSON.parse(localStorage.getItem(cacheKey));
-    }
+async function loadPokemonNames() {
     let offset = pokemonNames.length;
     let url = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`;
     let response = await fetch(url);
     let fetchedData = await response.json();
-    pokemonNames.push(...fetchedData['results']);
-    localStorage.setItem(cacheKey, JSON.stringify(pokemonNames));
+    for (let i = 0; i < fetchedData['results'].length; i++) {
+        const names = fetchedData['results'][i];
+        pokemonNames.push(names['name']);
+    }
 }
 
 
-async function loadSinglePokemon(pokemonNames) {
-    let cacheKey = `pokemon_details`;
-    if (localStorage.getItem(cacheKey)) {
-        console.log('Fetching from cache - Details');
-        pokemonDetails = JSON.parse(localStorage.getItem(cacheKey));
+async function loadPokemonDetails() {
+    for (let i = pokemonDetails.length; i < pokemonNames.length; i++) {
+        const pokemonName = pokemonNames[i];
+        let url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+        let response = await fetch(url)
+        let fetchedData = await response.json();
+        pokemonDetails.push(fetchedData)
     }
-    for (let i = 0; i < pokemonNames.length; i++) {
-        let url = `https://pokeapi.co/api/v2/pokemon/${pokemonNames[i]['name']}`;
+}
+
+
+async function loadPokemonSpecies() {
+    for (let i = pokemonSpecies.length; i < pokemonNames.length; i++) {
+        const pokemonName = pokemonNames[i];
+        let url = `https://pokeapi.co/api/v2/pokemon-species/${pokemonName}/`;
         let response = await fetch(url);
         let fetchedData = await response.json();
-        pokemonDetails.push(fetchedData);
-        localStorage.setItem(cacheKey, JSON.stringify(pokemonDetails));
+        pokemonSpecies.push(fetchedData);
     }
 }
 
-/*
-async function loadPokemonSpecies() {
-    let cacheKey = 'pokemon_species';
-    if (localStorage.getItem(cacheKey)) {
-        console.log('Fetching from cache - Species');
-        pokemonSpecies = JSON.parse(localStorage.getItem(cacheKey));
-    }
-    let url = `https://pokeapi.co/api/v2/pokemon-species/${}/`;
-    let response = await fetch(url);
-    let fetchedData = await response.json();
-    pokemonSpecies.push(fetchedData);
-    localStorage.setItem(cacheKey, JSON.stringify(pokemonSpecies));
-}*/
 
-async function render() {
+async function loadMore() {
+    await init();
+}
+
+
+function render() {
     let content = document.getElementById('content');
     content.innerHTML = '';
-
     for (let i = 0; i < pokemonNames.length; i++) {
-        const pokemon = pokemonNames[i];
-        content.innerHTML += `
-        <div onclick="openPokemonCard(${i})" id="pokemon-card${i}" class="pokemon-card">
-            <div class="pokemon-name-number-wrapper">
-                <div id="pokemon-name${i}" class="pokemon-name">${(pokemon['name']).charAt(0).toUpperCase() + pokemon['name'].slice(1)}</div>
-                <div id="pokemon-number${i}"></div>
-            </div>
-            <div><img class="pokemon-image" id="pokemon-image${i}"></div>
-            <div class="pokemon-types" id="pokemon-type${i}"></div>
-        </div>
-        `;
-    }
-    let type = document.getElementById(`pokemon-type${i}`);
-    let pokemonCard = document.getElementById(`pokemon-card${i}`);
-    type.innerHTML = '';
-    for (let j = 0; j < currentPokemon['types'].length; j++) {
-        const pkmType = currentPokemon['types'][j]['type'];
-        const mainType = pkmType['name'];
-        const secondaryType = pkmType['name'].charAt(0).toUpperCase() + pkmType['name'].slice(1);
-
-        type.innerHTML += `<div class="single-pokemon-type">${secondaryType}</div>`;
-        let typeElements = document.querySelectorAll(`#pokemon-type${i} div`);
-        typeElements.forEach((element) => {
-            if (element.textContent === secondaryType) {
-                element.classList.add(`${mainType}-type-main`);
-            }
-            let image = document.getElementById(`pokemon-image${i}`);
-            let number = document.getElementById(`pokemon-number${i}`);
-            image.src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
-            let formatedNumber = '#' + (currentPokemon['id'].toString().padStart(4, '0'));
-            number.innerHTML = formatedNumber;
-
-        });
-        pokemonCard.classList.add(`${currentPokemon['types'][0]['type']['name']}-type-secondary`);
+        generatePokemonCardContent(i);
     }
 }
 
 
+function generatePokemonCardContent(i) {
+    const name = pokemonNames[i];
+    const id = pokemonDetails[i]['id'];
+    const image = pokemonDetails[i]['sprites']['other']['official-artwork']['front_default'];
+    content.innerHTML += generatePokemonCardHTML(i, name, id, image);
+    addPokemonTypes(i);
+    addBackgroundColor(i);
+}
 
 
-/*     async function loadMore() {
-        await loadPokemon();
-        render();
+function addPokemonTypes(i) {
+    let pokemonTypes = document.getElementById(`pokemon-card-type${i}`);
+    pokemonTypes.innerHTML = '';
+    for (let j = 0; j < pokemonDetails[i]['types'].length; j++) {
+        const type = pokemonDetails[i]['types'][j];
+        pokemonTypes.innerHTML += `<div class="${type['type']['name']}-type-main single-pokemon-type">${type['type']['name']}</div>`;
+    }  
+}
+
+
+function addBackgroundColor(i) {
+    let pokemonCardBackground = document.getElementById(`pokemon-card${i}`);
+    for (let k = 0; k < pokemonDetails.length; k++) {
+        const mainType = pokemonDetails[i]['types'][0]['type']['name'];
+        pokemonCardBackground.classList.add(`${mainType}-type-secondary`)
     }
-
-    function closePokemonCard() {
-        let background = document.getElementById('single-pokemon-card');
-        if (background.className != '') {
-            background.className = '';
-        }
-        let overlay = document.getElementById('pokemon-modal');
-        let overlayBackground = document.getElementById('pokemon-background-modal');
-        overlayBackground.classList.add('d-none');
-        overlay.classList.add('d-none');
-    }
-
-    async function openPokemonCard(i) {
-        let overlay = document.getElementById('pokemon-modal');
-        let overlayBackground = document.getElementById('pokemon-background-modal');
-
-        overlayBackground.classList.remove('d-none');
-        overlayBackground.addEventListener('click', closePokemonCard)
-        overlay.classList.remove('d-none');
-        overlay.innerHTML = '';
-        let currentPokemonName = document.getElementById(`pokemon-name${i}`).innerText;
-        currentPokemon = await loadSinglePokemon(currentPokemonName.toLowerCase());
-        let pkmType = currentPokemon['types'][0]['type']['name'];
-        await loadPokemonDetails();
-        overlay.innerHTML += `
-        <div id="single-pokemon-card" class="single-pokemon-card">
-            <div class="single-pokemon-card-top">
-                <div class="card-top-buttons">
-                    <button id="left-button">&lt;</button>
-                    <button id="right-button">&gt;</button>
-                </div>
-
-                <div class="card-top-name">${currentPokemonName}</div>
-
-                <div class="card-top-type" id="card-top-type${i}}"></div>
-
-                <div class="card-top-number">${currentPokemon['id']}</div>
+}
 
 
-            </div>
-            <div class="single-pokemon-card-bottom">
-            <div class="card-bottom-image-container">
-            <img class="card-bottom-image" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}">
+function generatePokemonCardHTML(i, name, id, image) {
+    return `
+    <div onclick="openPokemonCard(${i})" class="pokemon-card" id="pokemon-card${i}">
+        <div class="pokemon-card-top">
+            <div>${name}</div>
+            <div>${id}</div>
         </div>
-                <div class="bottom-card-nav">
-                    <ul>
-                        <li onclick="showAboutSection()">About</li>
-                        <li onclick="showBaseSection()">Base Stats</li>
-                        <li onclick="showEvolutionSection()">Evolution</li>
-                        <li onclick="showMovesSection()">Moves</li>
-                    </ul>
-                <div class="flex-column" id="pokemon-card-content">
-                    <p>${(currentPokemonDetails['flavor_text_entries'][1]['flavor_text']).replace('/↑/g', "")}</p>
+        <div class="pokemon-card-bottom">
+            <div><img class="pokemon-card-image" src="${image}"></div>
+            <div class="pokemon-card-type" id="pokemon-card-type${i}"></div>
+        </div> 
+    </div>`;
+}
+
+
+function openPokemonCard(i) {
+    let modal = document.getElementById('pokemon-modal');
+    let backgroundModal = document.getElementById('pokemon-background-modal');
+    modal.classList.remove('d-none');
+    backgroundModal.classList.remove('d-none');
+    backgroundModal.addEventListener('click', closePokemonCard);
+    modal.innerHTML = '';
+    modal.innerHTML += generateSingleCardHTML(i);
+    console.log(i);
+    generateAboutSection(i);
+}
+
+
+function generateSingleCardHTML(i) {
+    return `
+        <div class="single-pokemon-card">
+            <div class="single-pokemon-card-top-section">
+                <div class="arrows">
+                <button>$</button>  <button></button>
                 </div>
-                </div>
+                <div class="top-section-name">${pokemonDetails[i]['name']}</div>
+                <div class="top-section-id">${pokemonDetails[i]['id']}</div>
+                <div class="top-section-type" id="top-section-type"></div>
+                <div class="top-section-image"><img src="${pokemonDetails[i]['sprites']['other']['official-artwork']['front_default']}"></div>
+            </div>
+        
+            <div class="single-pokemon-card-bottom-section">
+                <ul>
+                    <li onclick="showAboutSection(${i})">About</li>
+                    <li onclick="showBaseSection(${i})">Base Stats</li>
+                    <li onclick="showMovesSection(${i})">Moves</li>
+                </ul>
+                <div class="single-card-content" id="single-card-content"></div>
             </div>
         </div>
     `;
+}
 
-        let background = document.getElementById('single-pokemon-card');
-        background.classList.add(`${pkmType}-type-secondary`)
-    }
-
-
-    async function showAboutSection() {
-        let content = document.getElementById('pokemon-card-content');
-        content.classList.remove('moves', 'p-relative');
-        content.classList.add('flex-column');
-        content.innerHTML = '';
-        content.innerHTML += `
-    <div class="card-about-pokemon">
-    <div><b>Height: </b><p>${currentPokemon['height']}</p></div>
-    <div><b>Weight: </b><p>${currentPokemon['weight']}</p></div>
-    <div><b>Abilities:</b><p id="pokemon-abilities"></p></div>
-    <div class="about-egg-groups"><div><b>Egg Groups:</b></div><div id="pokemon-egg-group"></div></div>
-    </div>  
-    `;
-        let abilities = document.getElementById('pokemon-abilities');
-        abilities.innerHTML = ''
-        for (let i = 0; i < currentPokemon['abilities'].length; i++) {
-            const ability = currentPokemon['abilities'][i];
-            if (i === currentPokemon['abilities'].length - 1) {
-                abilities.innerHTML += `${ability['ability']['name']}`;
-            } else {
-                abilities.innerHTML += `${ability['ability']['name']}, `;
-            }
-        }
-        await showEggGroups();
-    }
-
-    function showBaseSection() {
-        let content = document.getElementById('pokemon-card-content');
-        content.classList.remove('moves', 'p-relative');
-        content.classList.add('flex-column');
-        content.innerHTML = '';
-        for (let i = 0; i < currentPokemon['stats'].length; i++) {
-            const stat = currentPokemon['stats'][i];
-            content.innerHTML += `
-        <div class="stat-progress-bar">
-            <div class="progress">
-                <div class="progress-bar" style="width: calc(${((stat['base_stat'] * 100) / 255)}%)" role="progressbar" aria-valuenow="${stat['base_stat']}" aria-valuemin="0" aria-valuemax="255">${stat['stat']['name']}: ${stat['base_stat']}</div>
-            </div>
-        </div>
-        `;
-        }
-    }
-
-    async function showEggGroups() {
-        let content = document.getElementById('pokemon-egg-group');
-        let eggGroup = await loadPokemonSpecies();
-        content.innerHTML = '';
-        for (let i = 0; i < eggGroup['egg_groups'].length; i++) {
-            const egg = eggGroup['egg_groups'][i];
-            if (i === eggGroup['egg_groups'].length - 1) {
-                content.innerHTML += `<span>${egg['name']}</span>`;
-            } else {
-                content.innerHTML += `<span>${egg['name']}, </spanY`;
-            }
-
-        }
-
-    }
-
-    async function showEvolutionSection() {
-        loadPokemonEvolution();
-    }
+function generateAboutSection(i) {
+    let content = document.getElementById('single-card-content');
+    content.innerHTML = '';
+    content.innerHTML +=  generateAboutHTML(i);
+}
 
 
-    function showMovesSection() {
-        let content = document.getElementById('pokemon-card-content');
-        content.classList.remove('flex-column');
-        content.classList.add('moves', 'p-relative');
-        let moves = currentPokemon['moves'];
-        content.innerHTML = '';
-
-        for (let i = 0; i < moves.length; i++) {
-            const move = moves[i];
-            content.innerHTML += `
-            <div class="pokemon-moves">${move['move']['name']}</div>
+function generateAboutHTML(i) {
+    return `
+            <div class="card-about">
+                <div>${pokemonSpecies[i]['flavor_text_entries'][10]['flavor_text']}</div>
+                <div><b>Height: </b><p>${pokemonDetails[i]['height']}</p></div>
+                <div><b>Weight: </b><p>${pokemonDetails[i]['weight']}</p></div>
+                <div><b>Abilities:</b><p id="pokemon-abilities"></p></div>
+                <div class="about-egg-groups"><div><b>Egg Groups:</b></div><div id="pokemon-egg-group"></div></div>
+            </div>  
             `;
-        }
-    } 
 }
-*/
+
+function generateBaseStats() {
+    let content = document.getElementById('single-card-content');
+    content.innerHTML = '';
+    content.innerHTML += ``;
+}
+
+function generateMoves() {
+    let content = document.getElementById('single-card-content');
+    content.innerHTML = '';
+    content.innerHTML += ``;
+}
+
+
+
+
+function closePokemonCard() {
+    let modal = document.getElementById('pokemon-modal');
+    let backgroundModal = document.getElementById('pokemon-background-modal');
+    modal.classList.add('d-none');
+    backgroundModal.classList.add('d-none');
+}
